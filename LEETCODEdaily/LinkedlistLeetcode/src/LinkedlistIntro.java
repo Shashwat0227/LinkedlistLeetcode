0f963137567068324938e6c9fabb1d5026c4614b
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-04-14 18:40:31 +0530

    commiting the first repo towards linkedlist questions and introduction will update this week with linked list only:) So give a star if you are satisfied with the questions and solutions :)

---
+++ LEETCODEdaily/LinkedlistLeetcode/src/LinkedlistIntro.java
@@ -0,0 +1,11 @@
+public class LinkedlistIntro {
+    public static void main(String[] args) {
+        System.out.println("LINked list intro in comments");
+    }
+}
+// Linked list is a form of sequential collection and it does not have to be in order.
+/*  Linked list is made up of independent nodes that may contain any type of data and each node has a reference to the next node
+in the link
+Elements of linked list are independent objects
+variable size - The size of the linked list is not predefined
+ */

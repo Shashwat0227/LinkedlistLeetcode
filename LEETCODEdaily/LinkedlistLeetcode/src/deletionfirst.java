1d2a2788c70d5bfe19c1fb09a41e1e01e4104624
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-04-18 21:33:22 +0530

    DISPLAYING HOW TO DELETE THE FIRST ELEMENT FROM THE NODE WITH EXPLAINATION

---
+++ LEETCODEdaily/LinkedlistLeetcode/src/deletionfirst.java
@@ -0,0 +1,12 @@
+
+//    public int DeleteFirst(){
+//        int value = head.value;
+//        head = head.next;
+//        if (head==null){
+//            tail=null;
+//        }
+//        size--;
+//        return value;
+//    }
+//}
+//here we will just put the head forward from the starting in order to remove the first element
